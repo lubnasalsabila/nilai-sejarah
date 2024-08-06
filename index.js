50794884ed1b2accb797d76187efdfa5d1c03fd1
lubna salsabila <sitilubnasalsabilamuslimah@smkwikrama.sch.id>
2024-08-06 10:33:39 +0700

    Pengayaan penambahan nilai sejarah

---
+++ index.js
@@ -0,0 +1,13 @@
+// Typed.js initialization
+document.addEventListener('DOMContentLoaded', function() {
+    // auto-typing
+    new Typed('.auto-type', {
+      strings: ['DIPONEGORO'],
+      typeSpeed: 200,
+      backSpeed: 50,
+      loop: true
+    });
+  });
+  
+  // ScrollReveal.js initialization
+  
